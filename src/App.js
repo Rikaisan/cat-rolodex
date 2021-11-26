7fd7c724bfc16730f7af7c95aef6e28274c94252
Rikaisan <rikaisensei@gmail.com>
2021-11-26 17:18:29 -0500

    Add data fetch and basic display

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+		};
+	}
+
+	async componentDidMount() {
+		let data = await fetch('https://jsonplaceholder.typicode.com/users');
+		data = await data.json();
+		this.setState({ monsters: data });
+		console.log(data);
+	}
+
 	render() {
-		return <div className='App'>Hello, world!</div>;
+		return (
+			<div className='App'>
+				{this.state.monsters.map((monster) => (
+					<div key={monster.id}>I am {monster.name}</div>
+				))}
+			</div>
+		);
 	}
 }
 
